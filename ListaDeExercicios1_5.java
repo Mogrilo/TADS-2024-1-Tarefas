21d248a48a2f2e1524422b1090c28da2d2bc9a46
Murilo Portela <160618849+Mogrilo@users.noreply.github.com>
2024-02-29 11:31:48 -0300

    Create ListaDeExercicios1_5.java

---
+++ ListaDeExercicios1_5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExerciciosTADS {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        float temperaturaC;
+        float temperaturaF;
+
+        System.out.println("digite a temperatura em graus Celsius : ");
+        temperaturaC = in.nextFloat(); 
+        temperaturaF = (9*temperaturaC/5 + 32); 
+
+        System.out.println(temperaturaF);
+        in.close();
+
+    }
+}
